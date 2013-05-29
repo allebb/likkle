01c0bd114d33129301b956c8e52b9c0ffe356c1a
Bobby Allen <bobbyallen.uk@gmail.com>
2013-05-29 13:28:09 +0100

    Committing initial code.

---
+++ src/Ballen/Likkle/Lk2inClient.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * Likkle
+ *
+ * Likkle is a composer compatible PHP client library for the lk2.in
+ *  URL shortener service (http://lk2.in).
+ *
+ * @author bobbyallen.uk@gmail.com (Bobby Allen)
+ * @version 1.0.0
+ * @license http://opensource.org/licenses/MIT
+ * @link https://github.com/bobsta63/likkle
+ * @link http://www.bobbyallen.me
+ *
+ */
+
+namespace Ballen\Likkle;
+
+class Lk2inClient
+{
+
+    const HTTP_LK2IN_URL = 'http://lk2.in/';
+
+}
+
+?>
